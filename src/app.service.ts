5c2795599ccc91884127e8fd32a8c51cdb37d58b
KDBismael <ismaelkondombo1@gmail.com>
2024-08-02 14:25:42 +0000

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,33 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { Response } from 'express';
+import { MessageDto } from './message.dto';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+  sendMessage(body: MessageDto, res: Response) {
+    let ticketCreation = {
+      userName: '',
+      email: '',
+      description: '',
+    }
+
+    switch (body.eventType) {
+      case "contact support":
+        return res.status(HttpStatus.OK).send({ sucess: true, message: "Entrez votre nom svp", haveFeedBack: false });
+      case "userName":
+        ticketCreation.userName = body.message;
+        return res.status(HttpStatus.OK).send({ sucess: true, message: "Entrez votre email svp", haveFeedBack: false });
+      case "email":
+        ticketCreation.email = body.message;
+        return res.status(HttpStatus.OK).send({ sucess: true, message: "Veillez entrez une description claire du probleme que vous rencontrez", haveFeedBack: false });
+      case "description":
+        ticketCreation.description = body.message;
+        return res.status(HttpStatus.OK).send({ sucess: true, message: "Voulez vous Confirmer la creation du ticket", haveFeedBack: false });
+      default:
+        return res.status(HttpStatus.OK).send({ sucess: true, message: "A message from our AI model", haveFeedBack: true });
+    }
+  }
+}
---
+++ src/message.dto.ts
@@ -0,0 +1,4 @@
+export class MessageDto {
+    eventType: string;
+    message: string;
+}
